cddaf3baf44e5cac0287299b85048074d752e2bb
Eunsung Lim <unsungvegetable@gmail.com>
2020-09-17 20:28:10 +0900

    implemented todos/new-todo page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+import TodoDetail from './components/TodoDetail/TodoDetail';
+import NewTodo from './containers/TodoList/NewTodo/NewTodo';
+import TodoList from './containers/TodoList/TodoList';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path='/todos' exact render={() => <TodoList title='My TODOs!' />} />
+          <Route path='/todos/:id' exact component={TodoDetail}/>
+          <Route path='/new-todo' exact component={NewTodo} />
+          <Redirect exact from='/' to='/todos' />
+          <Route render={() => <h1>Not Found</h1>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
